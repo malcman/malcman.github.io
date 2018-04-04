e28abf31dd2630f2606a881baf77dad5b5af1b30
Malcolm Maturen <malc@umich.edu>
2018-04-04 10:36:32 -0400

    added skills and skills images

--- js/main.js
+++ js/main.js
@@ -7,6 +7,15 @@ document.body.addEventListener("mouseup", function(e) {
 	}
 })
 
+document.body.addEventListener("touchend", function(e) {
+	var mobile = document.getElementsByClassName("mobile")[0];
+	var close = document.getElementById("close");
+	if ((e.target != mobile && e.target.parentNode != mobile)) {
+		mobile.classList.remove("leftmost")
+
+	}
+})
+
 function toggleMenu(){
 	var mobile = document.getElementsByClassName("mobile")[0];
 	var overlay = document.getElementsByClassName("overlay")[0];
