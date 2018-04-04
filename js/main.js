8cbf275944bf1920a09c3051b16ad9736331571a
Malcolm Maturen <malc@umich.edu>
2018-04-04 17:36:31 -0400

    Added resume page, finished projects

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@ document.body.addEventListener("mouseup", function(e) {
 	var mobile = document.getElementsByClassName("mobile")[0];
 	// var mobileSide = document.getElementsByClassName("mobile-sidebar")[0];
 	var close = document.getElementById("close");
-	var close = document.getElementById("close");
 	if ((e.target != mobile && e.target.parentNode != mobile)) {
 		mobile.classList.remove("leftmost")
-
 	}
 })
 
@@ -14,14 +12,13 @@ document.body.addEventListener("touchend", function(e) {
 	var close = document.getElementById("close");
 	if ((e.target != mobile && e.target.parentNode != mobile)) {
 		mobile.classList.remove("leftmost")
-
 	}
 })
 
 function toggleMenu(){
 	var mobile = document.getElementsByClassName("mobile")[0];
 	var overlay = document.getElementsByClassName("overlay")[0];
-	mobile.classList.toggle("leftmost");
-	overlay.classList.toggle("overlay-dark")
-
+	if (mobile) {
+		mobile.classList.toggle("leftmost");
+	}
 }
