eb0c7e419833d15dd9b9af2601d8cfa2e864cead
Malcolm Maturen <malc@umich.edu>
2018-04-06 03:12:43 -0400

    basic requirements completed

--- js/main.js
+++ js/main.js
@@ -1,24 +1,30 @@
 document.body.addEventListener("mouseup", function(e) {
 	var mobile = document.getElementsByClassName("mobile")[0];
-	// var mobileSide = document.getElementsByClassName("mobile-sidebar")[0];
 	var close = document.getElementById("close");
-	if ((e.target != mobile && e.target.parentNode != mobile)) {
-		mobile.classList.remove("leftmost")
+	if (window.innerWidth < 650) {
+		if ((e.target != mobile && e.target.parentNode != mobile)) {
+			mobile.classList.remove("leftmost")
+		}
 	}
 })
 
 document.body.addEventListener("touchend", function(e) {
 	var mobile = document.getElementsByClassName("mobile")[0];
 	var close = document.getElementById("close");
-	if ((e.target != mobile && e.target.parentNode != mobile)) {
-		mobile.classList.remove("leftmost")
+	var toggle = document.getElementById("toggle");
+	if (window.innerWidth < 650) {
+		if ((e.target != mobile && e.target.parentNode != mobile)) {
+			mobile.classList.remove("leftmost")
+		}
 	}
 })
 
 function toggleMenu(){
 	var mobile = document.getElementsByClassName("mobile")[0];
 	var overlay = document.getElementsByClassName("overlay")[0];
-	if (mobile) {
-		mobile.classList.toggle("leftmost");
+	if (window.innerWidth < 650) {
+		if (mobile) {
+			mobile.classList.toggle("leftmost");
+		}
 	}
 }
